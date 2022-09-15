8ed8fb75336c4ebb3c34f270b0b686da6b416ada
JHodgkins <jhodgkins78@gmail.com>
2022-09-15 17:07:11 +0000

    code: defined all products function and context

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
+""" import Products so the products can be viewed on the froneend """
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+def all_products(request):
+    """ Return all products including sorting and search results """
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
