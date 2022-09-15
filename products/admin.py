0f32ade31327d839263993e64a0ff90a4d921c89
JHodgkins <jhodgkins78@gmail.com>
2022-09-15 16:07:19 +0000

    code: admin dashboard tables display customised

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,28 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """ Amend the display of product items within the admin dashboard """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'certificate',
+        'no_discs',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ Amend the display of category items within the admin dashboard """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
