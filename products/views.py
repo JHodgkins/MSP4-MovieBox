9087353295de8e1aacb1bb26cf6cd785151d6062
JHodgkins <jhodgkins78@gmail.com>
2022-09-17 02:23:06 +0000

    code: search view updated and PEP8 errors fixed

--- products/views.py
+++ products/views.py
@@ -1,17 +1,38 @@
 """
 import render to render page and get object or 404 to handle if no item
 found when requested.
-import Products so the products can be viewed on the froneend
+import redirect and reverse to hanfle redirections of pages and rverse to
+handle unidirectional urls.
+import Products so the products can be viewed on the froneend.
+Import Q to handle wueries to locate search terms within page model areas.
 """
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product, Category
 
 
 def all_products(request):
     """ Return all products including sorting and search results """
     products = Product.objects.all()
+
+    search = None
+    if request.GET:
+        if 'search' in request.GET:
+            search = request.GET['search']
+            if not search:
+                messages.error(request, "You did not enter a search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=search) | Q(
+                description__icontains=search) | Q(
+                    actors__icontains=search) | Q(
+                        directed_by__icontains=search)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': search,
     }
     return render(request, 'products/products.html', context)
 
