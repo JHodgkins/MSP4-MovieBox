850c6deee215fae1c6b4ce0a13687e9e6b2addcd
JHodgkins <jhodgkins78@gmail.com>
2022-09-17 00:47:40 +0000

    code: added product details view and urls

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    
+    path('<product_id>', views.product_details, name='product_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,11 @@
-""" import Products so the products can be viewed on the froneend """
-from django.shortcuts import render
+"""
+import render to render page and get object or 404 to handle if no item
+found when requested.
+import Products so the products can be viewed on the froneend
+"""
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, Category
+
 
 def all_products(request):
     """ Return all products including sorting and search results """
@@ -8,5 +13,14 @@ def all_products(request):
     context = {
         'products': products,
     }
-
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ Return a single product using its id """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
