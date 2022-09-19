fa8fb8e679dbc3baded6439112dc5ea721073711
JHodgkins <jhodgkins78@gmail.com>
2022-09-19 03:38:13 +0000

    code: added doc strng and bag,items section

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,23 +1,40 @@
+"""
+import decimal to handle financial tranactions.
+import settings to refrence gloal variables set.
+import get object or 404 to handle returns from database.
+import Product model to access all table fields
+"""
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
     """ Construct basket contents and delivery value """
 
+    basket = request.session.get('basket', {})
     basket_items = []
     total = 0
     product_count = 0
 
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-    
     grand_total = delivery + total
-    
+
     context = {
         'basket_items': basket_items,
         'total': total,
