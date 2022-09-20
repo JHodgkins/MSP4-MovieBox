18c255bb7f5fbd5a9007a8ca687413f5632ee162
JHodgkins <jhodgkins78@gmail.com>
2022-09-20 03:06:14 +0000

    code: updated remove url for basket items

--- basket/urls.py
+++ basket/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<item_id>/', views.add_to_the_basket, name='add_to_the_basket'),
     path('update/<item_id>/', views.update_basket, name='update_basket'),
+    path('remove/<item_id>/', views.remove_from_the_basket, name='remove_from_the_basket'),
 ]
