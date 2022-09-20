5c5e5ec5835cb7a68414840b198c4ea080b6ae33
JHodgkins <jhodgkins78@gmail.com>
2022-09-20 00:43:44 +0000

    code: added url for handling update basket request

--- basket/urls.py
+++ basket/urls.py
@@ -9,4 +9,5 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<item_id>/', views.add_to_the_basket, name='add_to_the_basket'),
+    path('update/<item_id>/', views.update_basket, name='update_basket'),
 ]
