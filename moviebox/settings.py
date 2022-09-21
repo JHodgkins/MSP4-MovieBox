eff2c0fa613ce8ec57e8f186038c7a07e03c9452
JHodgkins <jhodgkins78@gmail.com>
2022-09-21 19:49:42 +0000

    code: completed migration models

--- moviebox/settings.py
+++ moviebox/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'homepage',
     'products',
     'basket',
+    'purchase',
 ]
 
 MIDDLEWARE = [
--- purchase/models.py
+++ purchase/models.py
@@ -14,7 +14,6 @@ from products.models import Product
 
 class Order(models.Model):
     """ Order model used on the purchase page to process an order """
-
     order_number = models.CharField(max_length=32, null=False, editable=False)
     date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -30,15 +29,59 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """
+        Generate a random 32 digit unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item order is added,
+        accounting for delivery threasholds.
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it has not been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """ Return the new item number as a string """
+        return self.order_number
+
 
 class LineItemOrder(models.Model):
-    """ 
-    line order item, used y the aadmin to read, create, edit and 
+    """
+    line order item, used y the aadmin to read, create, edit and
     delete individual customer orders
     """
 
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    grade = models.CharField(max_length=6, null=True, blank=True) # NEW-A, USED-A
+    grade = models.CharField(max_length=6, null=True, blank=True)  # NEW-A, USED-A
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """ Return SKU and Order number for each line item order """
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
