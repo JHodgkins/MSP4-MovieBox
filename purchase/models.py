1fd267d5d7da10222f8942646f1c7b0066c3208c
JHodgkins <jhodgkins78@gmail.com>
2022-09-21 18:02:49 +0000

    code: order and line order models created

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,44 @@
+"""
+Import uuid to generate a unique order number.
+import models so are available to foreign keys.
+import sum to calculate totals.
+import settings for use by models.
+import Products model so foreign keys can be used.
+"""
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    """ Order model used on the purchase page to process an order """
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class LineItemOrder(models.Model):
+    """ 
+    line order item, used y the aadmin to read, create, edit and 
+    delete individual customer orders
+    """
+
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=6, null=True, blank=True) # NEW-A, USED-A
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
