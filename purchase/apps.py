9d8ecd2604e48eaf5b8cae3288bd18705f28e0b2
JHodgkins <jhodgkins78@gmail.com>
2022-09-22 00:02:38 +0000

    code: added admin update and save signals

--- purchase/apps.py
+++ purchase/apps.py
@@ -1,6 +1,15 @@
+""" import apps configuration setttings """
 from django.apps import AppConfig
 
 
 class PurchaseConfig(AppConfig):
+    """ App configuration, ma,es the app """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'purchase'
+
+    def ready(self):
+        """
+        import signals into app so when a signal is recieved,
+        post_save and post_delete can execute.
+        """
+        import purchase.signals
---
+++ purchase/signals.py
@@ -0,0 +1,24 @@
+"""
+post_save and post_delete to save and delete when recieving a signal.
+Import recieiver so that data can be rieved from the admin dashboard.
+Import model LineItemOrder so fields can be interacted with.
+"""
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import LineItemOrder
+
+
+@receiver(post_save, sender=LineItemOrder)
+def update_order_total_on_save(sender, instance, created, **kwargs):
+    """
+    Update the line item order total when saved.
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=LineItemOrder)
+def update_order_total_on_delete(sender, instance, **kwargs):
+    """
+    Updates the ;ine item when an item is deleted using the admin dashoard.
+    """
+    instance.order.update_total()
