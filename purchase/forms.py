471f281e95b3f80c3ea62cc7ba4ff3e728a8973f
JHodgkins <jhodgkins78@gmail.com>
2022-09-22 16:26:19 +0000

    code: customer order form skeleton created

---
+++ purchase/forms.py
@@ -0,0 +1,42 @@
+"""
+Import forms into file.
+Import order model.
+"""
+from django import forms
+from .models import Order
+
+
+class CustomerOrderForm(forms.ModelForm):
+    """ Construct fields required on the customer form """
+    class Meta:
+        """
+        meta handles the fields to show on the form from
+        the order model
+        """
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Set the placeholders on fields and the class on
+        the Stripe widget field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone or Mobile Number',
+            'street_address1': 'Street Address Line 1',
+            'street_address2': 'Street Address Line 2',
+            'town_or_city': 'City, Town or Village',
+            'postcode': 'Postal Code',
+            'county': 'County',
+            'country': 'Country',
+        }
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
