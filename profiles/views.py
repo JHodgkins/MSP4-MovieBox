9ba21209e34c21fdfe8726cf39f2bf4c475c490c
JHodgkins <jhodgkins78@gmail.com>
2022-09-26 18:10:55 +0000

    code: added signals to create profile for registered users

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,21 @@
 """
 Import render to pass template files to be rendered
 to the frontend.
+Immport get object or 404 to handle requests that return null.
+Import user profile model so userr data can be accessed.
 """
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def profile(request):
     """
     View to display registered user profile to the frontend.
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/user_profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
