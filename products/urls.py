2fe9be86e655657ab6143088b455f1da38b3e1ea
JHodgkins <jhodgkins78@gmail.com>
2022-10-01 02:47:32 +0000

    code: updated edit urls

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_details, name='product_details'),
-    path('add-movie/', views.add_product, name='add_product'),
+    path('edit_movie/<int:product_id>/', views.edit_movie, name='edit_movie'),
+    path('add_movie/', views.add_movie, name='add_movie'),
 ]
