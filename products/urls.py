c415be30bd2cf8dcddb88f76d1e50906bef3736a
JHodgkins <jhodgkins78@gmail.com>
2022-10-01 03:33:46 +0000

    code: added delete movie urls

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_details, name='product_details'),
     path('edit_movie/<int:product_id>/', views.edit_movie, name='edit_movie'),
+    path('delete_movie/<int:product_id>/', views.delete_movie, name='delete_movie'),
     path('add_movie/', views.add_movie, name='add_movie'),
 ]
