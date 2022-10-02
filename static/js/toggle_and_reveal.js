f66804878ca8301b15780616037d1f53893783fc
JHodgkins <jhodgkins78@gmail.com>
2022-10-02 09:26:26 +0000

    fix: relocated core functional scripts

---
+++ static/js/toggle_and_reveal.js
@@ -0,0 +1,11 @@
+// Toggler function for mobile navigation -->
+    $(function () {
+    'use strict'
+
+    $('[data-toggle="offcanvas"]').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+        })
+    })
+
+// Toast reveal
+    $('.toast').toast('show');
