81acc2afb5647d8e6620c16a69c22d7fcc981767
JHodgkins <jhodgkins78@gmail.com>
2022-10-07 10:05:04 +0000

    code: updated dj database import

--- moviebox/settings.py
+++ moviebox/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from pathlib import Path
 import dj_database_url
+from pathlib import Path
 if os.path.exists("env.py"):
     import env  #noqa
 
