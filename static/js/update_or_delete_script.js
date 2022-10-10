07f382f4d184ee5fa49356ae2637c789e61e50c7
JHodgkins <jhodgkins78@gmail.com>
2022-10-10 01:59:38 +0000

    fix: moved to inline script because of csrf token

--- static/js/update_or_delete_script.js
+++ static/js/update_or_delete_script.js
@@ -1,19 +1,19 @@
 // Update quantity on basket page
-$('.update-link').click(function(e) {
-    var form = $(this).prev('.update-form');
-    form.submit();
-})
+    $('.update-link').click(function(e) {
+        var form = $(this).prev('.update-form');
+        form.submit();
+    })
 
-// Remove item from basket and reload page
-$('.remove-item').click(function(e) {
-    var csrfToken = "{{ csrf_token }}";
-    var itemId = $(this).attr('id').split('remove_')[1];
-    var item = $(this).data('remove');
-    var url = `/basket/remove/${itemId}/`;
-    var data = {'csrfmiddlewaretoken': csrfToken, 'remove': item};
+    // Remove item from basket and reload page
+    $('.remove-item').click(function(e) {
+        var csrfToken = "{{ csrf_token }}";
+        var itemId = $(this).attr('id').split('remove_')[1];
+        var item = $(this).data('remove');
+        var url = `/basket/remove/${itemId}/`;
+        var data = {'csrfmiddlewaretoken': csrfToken, 'remove': item};
 
-    $.post(url, data)
-        .done(function() {
-            location.reload();
-        });
-})
+        $.post(url, data)
+         .done(function() {
+             location.reload();
+         });
+    })
